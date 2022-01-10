2c844d04e6a49674375b239f600df0d14529a44f
DanielLinux <danielcarvalhorst@gmail.com>
2022-01-10 14:32:33 -0300

    implementada Check e Uncheck de habitos

--- src/HojePage/HojePage.js
+++ src/HojePage/HojePage.js
@@ -19,6 +19,7 @@ else{
 var semana = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado"];
 
 export default function HojePage(){
+
   const navigate = useNavigate();
   const { token } = useContext(UserContext);
   const [habits,setHabits]=useState(null)
@@ -26,13 +27,70 @@ export default function HojePage(){
     const promisse2 = axios.get(
     "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/today",
     {headers: { Authorization: `Bearer ${token}`}})
-    promisse2.then(response=>{setHabits(response.data);console.log(response.data)})
+    promisse2.then(response=>{
+      
+        for(let i=0;i<response.data.length;i++){console.log("cria")}
+      
+      setHabits(response.data);
+      console.log(response.data)
+    })
     console.log(habits)
     promisse2.catch(e=>{alert("logue novamente"); navigate("/")})
 },[])
   
   var date = dayjs().locale('PT-BR').format('DD/MM')
   var week =semana[dayjs().format('d')]
+
+  function doneHabit(id) {
+    // console.log(id)
+    let [done,newid] =id.split(',')
+    
+    console.log(newid+"+"+done)
+    if((done=="true")){
+      const promisseUncheck = axios.post(
+        `https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/${newid}/uncheck`,
+        {},
+        {headers: { Authorization: `Bearer ${token}`}
+         })
+      promisseUncheck.then((response)=>{console.log(response.data);reload()});
+      promisseUncheck.catch((e)=>console.log(e.response))
+    }
+    else{
+      const promisseCheck = axios.post(
+        `https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/${newid}/check`,
+        {},
+        {headers: { Authorization: `Bearer ${token}`}
+         })
+      promisseCheck.then((response)=>{console.log(response.data);reload()});
+      promisseCheck.catch((e)=>console.log(e))
+    }
+  }
+  function reload(){
+    const promisse2 = axios.get(
+      "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/today",
+      {headers: { Authorization: `Bearer ${token}`}})
+      promisse2.then(response=>{setHabits(response.data);console.log(response.data)})
+      console.log(habits)
+      promisse2.catch(e=>{alert("logue novamente"); navigate("/")})
+  }
+
+  function Habit({name,now,record,done,id}){
+    return(
+      <>
+      <HabitContainer>
+      <div>
+        <h1>{name}</h1>
+        <p>Sequencia Atual: {now} dias</p>  
+        <p>Seu recorde: {record} dias</p>  
+      </div>
+      <Check  disabled={true} id={`${done},${id}`}  onClick={(event)=>{doneHabit(event.target.id)}} $state={done}>
+      <ion-icon disabled={true} id={`${done},${id}`}  name="checkmark-outline"></ion-icon>
+      </Check>
+      </HabitContainer>
+      </>
+    )
+  }
+
 
     return(
     <>
@@ -43,8 +101,8 @@ export default function HojePage(){
             <p>{week}, {date}</p> 
             <p className="sub-text">67% dos hábitos concluídos</p>
            </SubBar>
-           {habits==null||habits.length==0?<VoidText>Você não tem nenhum hábito cadastrado para hoje. Adicione um hábito para começar a trackear!</VoidText>:habits.map((item,index)=> <Habit key={index} name={item.name} now={item.currentSequence} record={item.highestSequence} done={item.done}/> )}
-          
+           {habits==null||habits.length==0?<VoidText>Você não tem nenhum hábito cadastrado para hoje. Adicione um hábito para começar a trackear!</VoidText>:habits.map((item,index)=> <Habit key={index} name={item.name} id={item.id} now={item.currentSequence} record={item.highestSequence} done={item.done}/> )}
+            {console.log(habits)}
         </Frame>
       <BottomBar/>
         
@@ -52,19 +110,3 @@ export default function HojePage(){
     )
 }
 
-function Habit({name,now,record,done}){
-  return(
-    <>
-    <HabitContainer>
-    <div>
-      <h1>{name}</h1>
-      <p>Sequencia Atual: {now} dias</p>  
-      <p>Seu recorde: {record} dias</p>  
-    </div>
-    <Check $state={done}>
-    <ion-icon name="checkmark-outline"></ion-icon>
-    </Check>
-    </HabitContainer>
-    </>
-  )
-}
